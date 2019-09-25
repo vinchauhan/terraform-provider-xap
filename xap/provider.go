5e2975f6514e08953588a11eab867420dd6f8bdf
vchauhan <vchauhan@gmail.com>
2019-09-25 00:22:04 -0700

    Setup provider

--- xap/provider.go
+++ xap/provider.go
@@ -1,5 +1,33 @@
 package xap
 
-func Provider() {
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
 
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("XAP_API_URL", ""),
+			},
+			"user": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("XAP_USER", ""),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("XAP_PASSWORD", ""),
+			},
+			"skip_ssl_validation": &schema.Schema{
+				Type:        schema.TypeBool,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("XAP_SKIP_SSL_VALIDATION", "true"),
+			},
+		},
+	}
 }
